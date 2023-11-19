381f278dec1be749843cb78b113b16e307a7d309
Gerdeson de souza <gerdesondesouzapinto@gmail.com>
2023-11-19 12:41:03 -0300

    add os componentes presentes

--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -1,11 +1,12 @@
 import Menu from "../components/Menu";
+import Presente from "../components/Presente";
 import { BotaoCadastro, Fundo } from "../styles/Inicio";
 
 export default function Inicio() {
     return (
         <Fundo>
             <Menu />
-
+            <Presente />
             <span> Lista de aniversario inicio </span>
             <h3>cadastro de quem vai dar o presente</h3>
             <BotaoCadastro> Cadastrar </BotaoCadastro>
